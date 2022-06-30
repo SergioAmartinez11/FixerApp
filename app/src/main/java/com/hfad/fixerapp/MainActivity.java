7fcaed2ceb7079e61c74310af86e5f8c2c009d0d
Sergio <alonso.martinez64@uabc.edu.mx>
2022-06-29 22:02:56 -0700

    Forth advance

--- app/src/main/java/com/hfad/fixerapp/MainActivity.java
+++ app/src/main/java/com/hfad/fixerapp/MainActivity.java
@@ -3,30 +3,18 @@ package com.hfad.fixerapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.google.android.gms.common.api.Api;
-
+import androidx.appcompat.widget.Toolbar;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
-
 import java.io.IOException;
 import java.util.ArrayList;
-
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -38,8 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private final OkHttpClient client = new OkHttpClient();
     private String FROM_CURRENCY = "MXN";
     ArrayList<TextView> textViews = new ArrayList<>();
-    private TextView secondCard;
-    private TextView gpsTextView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,18 +39,27 @@ public class MainActivity extends AppCompatActivity {
         textViews.add((TextView) findViewById(R.id.info_text4));
         textViews.add((TextView) findViewById(R.id.info_text5));
         textViews.add((TextView) findViewById(R.id.info_text6));
-        gpsTextView = (TextView) findViewById(R.id.text_gps);
-        cardRequest();
-
-        int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
-        if(permissionCheck == PackageManager.PERMISSION_DENIED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
-                //EXPLICACION DE PERMISO DENEGADO ASINCRONO
+        textViews.add((TextView) findViewById(R.id.info_text7));
+        textViews.add((TextView) findViewById(R.id.info_text8));
+        toolbar = (Toolbar) findViewById(R.id.Toolbar);
+        toolbar.inflateMenu(R.menu.main_menu);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.usd_def){FROM_CURRENCY = "USD";}
+                else if(id == R.id.mxn_def){FROM_CURRENCY = "MXN";}
+                else if(id == R.id.cad_def){FROM_CURRENCY = "CAD";}
+                else if(id == R.id.gbp_def){FROM_CURRENCY = "GBP";}
+                else if(id == R.id.eur_def){FROM_CURRENCY = "EUR";}
+                else if(id == R.id.jpy_def){FROM_CURRENCY = "JPY";}
+                else if(id == R.id.cnd_def){FROM_CURRENCY = "CND";}
+                else if(id == R.id.hkd_def){FROM_CURRENCY = "HKD";}
+                cardRequest();
+                return false;
             }
-        }else{
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
-        }
-
+        });
+        cardRequest();
 
     }
 
@@ -77,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
     private void cardRequest(){
         //LOGICA DE CONEXION A LA API
         Request request = new Request.Builder()
-                .url("https://api.apilayer.com/fixer/latest?symbols=USD%2CEUR%2CJPY%2CCAD%2CCNY%2CHKD&base="+FROM_CURRENCY)
+                .url("https://api.apilayer.com/fixer/latest?symbols=MXN%2CGBP%2CUSD%2CEUR%2CJPY%2CCAD%2CCNY%2CHKD&base="+FROM_CURRENCY)
                 .addHeader("apikey","GoWHRWa5IvUGnjDsz4xvPco6kh8Hsze0").build();
         client.newCall(request).enqueue(new Callback() {
             @Override
@@ -98,12 +94,14 @@ public class MainActivity extends AppCompatActivity {
                         conversions.add( new Conversion(conversionJSON.getString("base"),"JPY","1",ratesJSON.getString("JPY")));
                         conversions.add( new Conversion(conversionJSON.getString("base"),"CNY","1",ratesJSON.getString("CNY")));
                         conversions.add( new Conversion(conversionJSON.getString("base"),"HKD","1",ratesJSON.getString("HKD")));
+                        conversions.add( new Conversion(conversionJSON.getString("base"),"MXN","1",ratesJSON.getString("MXN")));
+                        conversions.add( new Conversion(conversionJSON.getString("base"),"GBP","1",ratesJSON.getString("GBP")));
 
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                for(int i = 0; i<6;i++){
-                                    String newString = conversions.get(i).getResult()+"\t"+conversions.get(i).getFrom()
+                                for(int i = 0; i<8;i++){
+                                    String newString = conversions.get(i).getResult()+"    "+conversions.get(i).getFrom()
                                             +" -> "+conversions.get(i).getTo();
                                     textViews.get(i).setText(newString);
                                 }
